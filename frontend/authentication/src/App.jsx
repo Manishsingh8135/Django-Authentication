2f0b1824892b0504f97c84eaee00ad49cf1aadce
Manishsingh8135 <manishsingh8135@gmail.com>
2023-04-27 07:08:12 +0530

    frontend setupt

--- frontend/authentication/src/App.jsx
+++ frontend/authentication/src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import Header from './components/Header'
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2>Hello world</h2>
+      <Header/>
+      <Routes>
+          <Route path='/' exact Component={HomePage}/>
+          <Route path='/login' Component={LoginPage}/>
+      </Routes>
     </>
   )
 }
---
+++ frontend/authentication/src/pages/LoginPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+        <form action="">
+            <input type="text" name='username' placeholder='username' />
+            <input type="password" name="password" placeholder='password'  />
+            <input type="submit"  />
+        </form>
+
+
+    </div>
+  )
+}
+
+export default LoginPage
